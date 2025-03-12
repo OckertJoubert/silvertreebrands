b30fa69cd799e3600c3b7130c62681ab4e8e88a9
Trapsikies <trapsikies@gmail.com>
2025-03-12 11:31:44 +0200

    init(next.js): initial commit

---
+++ react-funtional-button/pages/components/cycleButton.tsx
@@ -0,0 +1,37 @@
+import React, { FC, useState } from "react";
+import styles from "@/styles/Home.module.css";
+
+interface CycleButtonProps {
+  colors: string[];
+}
+
+const CycleButton: FC<CycleButtonProps> = ({ colors }) => {
+  const [currentColorIndex, setCurrentColorIndex] = useState(0);
+
+  const handleClick = () => {
+    setCurrentColorIndex((prevIndex) => (prevIndex + 1) % colors.length);
+  };
+
+  return (
+    <>
+      <div className={styles.ctas}>
+        <a
+          className={styles.primary}
+          target="_blank"
+          rel="noopener noreferrer"
+          style={{
+            backgroundColor: colors[currentColorIndex],
+            borderColor:
+              colors[currentColorIndex] === "white" ? "black" : "white",
+            borderWidth: "2px",
+          }}
+          onClick={handleClick}
+        >
+          Cycle color
+        </a>
+      </div>
+    </>
+  );
+};
+
+export default CycleButton;
